0510f0769d737faeb92ef1e837e8b5b7b68ece6a
vulture20 <vulture20@web.de>
2015-02-13 09:48:36 +0100

    First commit

---
+++ USBRelais/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace USBRelais
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(48, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Toggle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "On";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(48, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Off";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(48, 130);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(88, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Relais-Status";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(182, 181);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "USBRelais";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ USBRelais/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Main;
+
+namespace USBRelais
+{
+    public partial class Form1 : Form
+    {
+        public static UsbDevice MyUsbDevice;
+        public static UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x16C0, 0x05DC);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean getStatus()
+        {
+            int transferred;
+            byte[] buffer = new byte[1] { 0x00 };
+
+            UsbSetupPacket setupPkt = new UsbSetupPacket((byte)(UsbCtrlFlags.Recipient_Interface | UsbCtrlFlags.RequestType_Vendor | UsbCtrlFlags.Direction_In), 4, 0, 0, 0);
+            MyUsbDevice.ControlTransfer(ref setupPkt, buffer, buffer.Length, out transferred);
+            if (transferred == 1)
+            {
+                return (buffer[0] == 0);
+            }
+            return false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+                if (MyUsbDevice == null) throw new Exception("Device not Found.");
+                timer1.Enabled = true;
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int transferred;
+            byte[] buffer = new byte[1] { 0x00 };
+
+            UsbSetupPacket setupPkt = new UsbSetupPacket((byte) (UsbCtrlFlags.Recipient_Interface | UsbCtrlFlags.RequestType_Vendor | UsbCtrlFlags.Direction_In), 1, 0, 0, 0);
+            MyUsbDevice.ControlTransfer(ref setupPkt, buffer, buffer.Length, out transferred);
+            checkBox1.Checked = getStatus();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            checkBox1.Checked = getStatus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int transferred;
+            byte[] buffer = new byte[1] { 0x00 };
+
+            UsbSetupPacket setupPkt = new UsbSetupPacket((byte)(UsbCtrlFlags.Recipient_Interface | UsbCtrlFlags.RequestType_Vendor | UsbCtrlFlags.Direction_In), 2, 0, 0, 0);
+            MyUsbDevice.ControlTransfer(ref setupPkt, buffer, buffer.Length, out transferred);
+            checkBox1.Checked = getStatus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int transferred;
+            byte[] buffer = new byte[1] { 0x00 };
+
+            UsbSetupPacket setupPkt = new UsbSetupPacket((byte)(UsbCtrlFlags.Recipient_Interface | UsbCtrlFlags.RequestType_Vendor | UsbCtrlFlags.Direction_In), 3, 0, 0, 0);
+            MyUsbDevice.ControlTransfer(ref setupPkt, buffer, buffer.Length, out transferred);
+            checkBox1.Checked = getStatus();
+        }
+    }
+}
